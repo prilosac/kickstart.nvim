f0b811ee57fc2d1dccd1b50e0f6c6f6290582c12
Lucas Teixeira <prilosac@gmail.com>
2024-04-29 14:29:26 -0700

    add in commented out config to disable python type checking, since it dies with Django

--- init.lua
+++ init.lua
@@ -612,7 +612,15 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = {},
+        pyright = {
+          -- settings = {
+          --   python = {
+          --     analysis = {
+          --       typeCheckingMode = 'off',
+          --     },
+          --   },
+          -- },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
